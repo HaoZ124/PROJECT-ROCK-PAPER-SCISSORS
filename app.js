ebb589251eb0ae6bbe90cf10a1e882e51c55eb88
HaoZ124 <zhouhao927@gmail.com>
2022-06-27 12:12:46 -0600

    Wrote the algorithm for js

---
+++ app.js
@@ -0,0 +1,5 @@
+//To save the user's input
+//To create the array of input for computer
+//Function for the computer to pick one
+//Function to compare the two inputs and return game result
+//Function to record the game scores and bo5
